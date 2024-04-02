78906295f4dcceef41e90a45097e0e8cf6ea0912
Cassandra Juif <cassandra.juif@gmail.com>
2024-04-02 19:24:39 +0200

    feat: setting up the project and creating header and footer

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+
+import { SliceZone } from "@prismicio/react";
+import * as prismic from "@prismicio/client";
+
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const home = await client.getByUID("page", "home");
+
+  return {
+    title: prismic.asText(home.data.title),
+    description: home.data.meta_description,
+    openGraph: {
+      title: home.data.meta_title ?? undefined,
+      images: [{ url: home.data.meta_image.url ?? "" }],
+    },
+  };
+}
+
+export default async function Index() {
+  // The client queries content from the Prismic API
+  const client = createClient();
+  const home = await client.getByUID("page", "home");
+
+  return <SliceZone slices={home.data.slices} components={components} />;
+}
